6b2b5328aa82c461c81fe9cc55ca99923b090334
Ryan Pavlik <rpavlik@iastate.edu>
2011-08-01 09:48:04 -0500

    Export rubylib

--- driver.sh
+++ driver.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-export SVN2GIT="RUBYLIB=$WORKSPACE/svn2git/lib ruby $WORKSPACE/svn2git/bin/svn2git"
+export RUBYLIB=$WORKSPACE/svn2git/lib
+export SVN2GIT="ruby $WORKSPACE/svn2git/bin/svn2git"
 export CLONEDIR="$WORKSPACE/clonedir"
 export CLONEDSTAMP="$WORKSPACE/cloned-stamp"
 $WORKSPACE/init.sh $@
